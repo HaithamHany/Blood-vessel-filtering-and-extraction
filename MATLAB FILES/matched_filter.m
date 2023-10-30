0c7f93fb0b63f8c9a3d3675e107e195f92c7cf03
Haitham Abdel-Salam <haithamhany7@gmail.com>
2023-10-30 17:09:27 -0400

    Updating function to use adaptiv histogram equalization to enhance contrast

--- MATLAB FILES/matched_filter.m
+++ MATLAB FILES/matched_filter.m
@@ -1,19 +1,29 @@
-I = imread('Vessels-test.png');
+I = imread('Vessle-Test02.png');
 if size(I, 3) == 3
     I = rgb2gray(I); % Convert to grayscale if the image is RGB
 end
-I = double(I);
+
+% Contrast Enhancement using Adaptive Histogram Equalization
+I_uint8 = im2uint8(I); % Convert to uint8 for adapthisteq
+I_enhanced_uint8 = adapthisteq(I_uint8,'ClipLimit',0.02); 
+I = im2double(I_enhanced_uint8); % Convert back to double for further processing
 
 s = 0.5;
-L = 1; 
+L = 0.5; 
 filter = gaussian_matched_filter(s, L);
 
 % Apply  filter
 response = conv2(I, filter, 'same');
 
+% Background subtraction
+se = strel('disk', 15);
+background = imopen(response, se);
+vessel_enhanced = response - background;
+
+% Display
 figure;
 subplot(1,2,1), imshow(I, []), title('Original Image');
-subplot(1,2,2), imshow(response, []), title('Matched Filter');
+subplot(1,2,2), imshow(vessel_enhanced, []), title('Vessel Enhanced');
 
 function f = gaussian_matched_filter(s, L)
     % s is the scale of the filter
